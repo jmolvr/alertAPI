f3424b5751fe9b5818300c5aa33371699807524a
João Marcos <joaosantana1998@gmail.com>
2019-11-12 01:04:24 -0300

    Update settings.py

--- map_alert/settings.py
+++ map_alert/settings.py
@@ -17,7 +17,7 @@ load_dotenv()
 # configure Django app for heroku
 import django_heroku
 
-django_heroku.settings(locals(),  staticfiles=False)
+django_heroku.settings(locals(), staticfiles=False, databases=False)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
