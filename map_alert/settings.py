2ff967e4519787d6719a38716561421473d8c7cf
João Marcos <joaosantana1998@gmail.com>
2019-11-30 00:15:52 -0300

    Update settings.py

--- map_alert/settings.py
+++ map_alert/settings.py
@@ -73,8 +73,9 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = list(default_methods)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staticfiles/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 ROOT_URLCONF = "map_alert.urls"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
