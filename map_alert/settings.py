c9de544b7a83bc635619b0bc210c96bdef4836c9
João Marcos <joaosantana1998@gmail.com>
2019-11-29 23:24:37 -0300

    Deploy Heroku 2

--- map_alert/settings.py
+++ map_alert/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://mapalertunifapapi.herokuapp.com/"]
+ALLOWED_HOSTS = ["mapalertunifapapi.herokuapp.com"]
 
 
 # Application definition
