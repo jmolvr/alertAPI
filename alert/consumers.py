31b5e62ea3608f3ab9ee04e825bbac11584fa65f
João Marcos <joaosantana1998@gmail.com>
2019-12-03 23:46:13 -0300

    URL de não resolvidos
    Gerado para que se mostre apenas os alertas que estão no estado de não resolvido.

--- alert/consumers.py
+++ alert/consumers.py
@@ -9,17 +9,18 @@ from django.dispatch import receiver
 
 class AlertConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        group_name = AlertSerializer.get_group()
-
+        group_name = AlertSerializer.get_group()  # apenas para padronizar
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = group_name + "_" + self.room_name
         await self.channel_layer.group_add(
-            group_name,
+            self.room_group_name,
             self.channel_name
         )
         await self.accept()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
-            AlertSerializer.get_group(),
+            self.room_group_name,
             self.channel_name
         )
 
@@ -34,17 +35,38 @@ class AlertConsumer(AsyncJsonWebsocketConsumer):
 @receiver([post_save, post_delete], sender=Alert)
 def update_alerts(sender, instance, **kwargs):
     alertas = Alert.objects.order_by('-created_at')
+    alertas_unsolved = alertas.filter(status=0).order_by("-created_at")
+
     serializer = AlertSerializer(alertas, many=True)
-    group_name = AlertSerializer.get_group()
+    serializer_unsolved = AlertSerializer(alertas_unsolved, many=True)
+
+    group_name = "alertas_todos"
+    group_name_unsolved = "alertas_unsolved"
+
     channel_layer = get_channel_layer()
+
     content = {
         "type": "UPDATE_ALERTS",
         "payload": serializer.data
     }
 
+    content_unsolved = {
+        "type": "UPDATE_ALERTS",
+        "payload": serializer_unsolved.data
+    }
+
     async_to_sync(channel_layer.group_send)(
-        group_name, {
+        group_name,
+        {
             "type": "notify",
             "content": content,
+        }
+    )
+
+    async_to_sync(channel_layer.group_send)(
+        group_name_unsolved,
+        {
+            "type": "notify",
+            "content": content_unsolved,
         }
     )
--- alert/routing.py
+++ alert/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from alert import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/alertas/', consumers.AlertConsumer)
+    re_path(r'ws/alertas/(?P<room_name>\w+)/$', consumers.AlertConsumer)
 ]
